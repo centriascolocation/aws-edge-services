2f2775c15d23090119dc51ca959c5ca7e79c0067
Volker Bohne <bohne@centrias-colocation.de>
2021-04-13 14:25:40 +0200

    locals for tags

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,6 @@
-/*
 locals {
-  region = var.environment["default_region"]
   common_tags = {
     Generator = "Terraform"
     Module    = "de.centrias.aws.terraform.modules.accounts.edge-services"
   }
 }
-*/
