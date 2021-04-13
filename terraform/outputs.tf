3257dfc41743e5dc5cdbbb66c46f4735d87f3610
Volker Bohne <bohne@centrias-colocation.de>
2021-04-13 13:46:17 +0200

    output content bucket name added

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,7 @@ output "lambda_function_arn" {
   value       = module.lambdaedge_function.lambda_function_arn
   description = "arn of lambda@edge function"
 }
+output "content_bucket_name" {
+  value       = module.cdn_bucket.bucket_name
+  description = "name of content bucket"
+}
