f9bde8430be2d195795a97c70d241e704fad01f1
Volker Bohne <bohne@centrias-colocation.de>
2021-04-13 11:57:50 +0200

    tags for bucket and access logging

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -1,6 +1,12 @@
 module "s3_bucket" {
-  source      = "centriascolocation/security/aws//modules/secure-s3-bucket"
-  version     = "1.5.0"
-  bucket_name = "${var.config["apex_domain"]}-cdn-content"
-  enabled     = true
+  source                            = "centriascolocation/security/aws//modules/secure-s3-bucket"
+  version                           = "1.5.0"
+  bucket_name                       = "${var.config["apex_domain"]}-cdn-content"
+  tags                              = local.common_tags
+  lifecycle_glacier_transition_days = 768
+  log_bucket_name                   = "${var.config["apex_domain"]}-cdn-accesslogs"
+  logging = {
+    target_bucket = "${var.config["apex_domain"]}-cdn-accesslogs"
+    target_prefix = "bucket_accesslogs/"
+  }
 }
